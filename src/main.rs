82e444aec4d04417f26c3aa1f5c851a30458a0ec
Adam Young <ayoung@redhat.com>
2020-09-18 13:00:25 -0400

    formatting cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use clap::Clap;
 
 mod dhcp;
-/// This doc string acts as a help message when the user runs '--help'
-/// as do all doc strings on fields
+/// run the rustboot server
 #[derive(Clap)]
 #[clap(version = "1.0", author = "Adam Young <adam@younglogic.com>")]
 struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
+    /// Sets a custom config file. 
     #[clap(short, long, default_value = "default.conf")]
     config: String,
 
@@ -22,14 +21,11 @@ struct Opts {
     /// If the server should write captured packets to disk
     #[clap(short)]
     write_capture: bool,
-
 }
 
 fn main() -> std::io::Result<()> {
 
     let opts: Opts = Opts::parse();
-
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
     println!("Value for config: {}", opts.config);
     let server = dhcp::DHCPServer::new( opts.verbose > 0,
                                         opts.write_capture,
